523bbc7ea7453ddb1de9fe41c8e94d26be0b5e38
Eremia Mihai Daniel <mihaitzadaniel2@gmail.com>
2018-08-07 16:52:25 +0300

    Extensie
    Add ext

---
+++ Google Extension/public/worker.js
@@ -0,0 +1,173 @@
+
+var localstorage=[];
+var lastTabId;
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+if(tab.status=="complete")
+{
+  var url = new URL(tab.url)
+  var time = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+
+  if(localstorage[tabId].url==null){
+    var data=[];
+    data['time']=time;
+    data['url']=tab.url;
+    data['host']=url.hostname;
+    localstorage[tabId]=data;
+    lastTabId=tabId;
+    console.log("Am intrat pe domeniul: "+url.hostname + " la adresa: "+tab.url);
+  }
+  else if(localstorage[tabId].url!==tab.url)
+  {
+    var open = localstorage[tabId].time;
+    var close  =  Date.now();
+    var diff = close - open;
+    console.log("Am inchis "+ localstorage[tabId].url + " si intru pe "+tab.url+ " dupa "+ diff/1000);
+      chrome.storage.local.get('userid', function(id) {
+        var data={
+        open: open,
+        close: close,
+        diff: (diff/1000),
+        url:localstorage[tabId].url,
+        host:localstorage[tabId].host,
+        userid: id.userid
+      };
+      (async () => {
+        const rawResponse = await fetch('http://localhost/api/close', {
+          method: 'POST',
+          headers: {
+            //'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        });
+      })();
+    });
+    var data=[];
+    data['time']=time;
+    data['url']=tab.url;
+    data['host']=url.hostname;
+    localstorage[tabId]=data;
+    lastTabId=tabId;
+  }
+  else
+  {
+    console.log("Am dat refresh la adresa: "+tab.url);
+  }
+
+}
+});
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  chrome.tabs.get(activeInfo.tabId, function(tab){
+    var tabid = activeInfo.tabId;
+     var url = new URL(tab.url)
+     var time = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+     console.log("Am intrat pe domeniul: "+url.hostname + " la adresa: "+tab.url);
+     if(localstorage[tabid]==null || localstorage[lastTabId]==null  || localstorage[lastTabId].url==null || localstorage[lastTabId].host==null)
+     {
+      var data=[];
+      data['time']=time;
+      data['url']=tab.url;
+      data['host']=url.hostname;
+      localstorage[tabid]=data;
+      lastTabId=tabid;
+     }
+     else if(localstorage[lastTabId].url!==url)
+     {
+        var open = localstorage[lastTabId].time;
+        var close  =  Date.now();
+        var diff = close - open;
+          chrome.storage.local.get('userid', function(id) {
+            var data={
+            open: open,
+            close: close,
+            diff: (diff/1000),
+            url:localstorage[lastTabId].url,
+            host:localstorage[lastTabId].host,
+            userid: id.userid
+          };
+          lastTabId=tabid;
+          (async () => {
+            const rawResponse = await fetch('http://localhost/api/close', {
+              method: 'POST',
+              headers: {
+             //   'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            });
+          })();
+        });
+        var data=[];
+        data['time']=time;
+        data['url']=tab.url;
+        data['host']=url.hostname;
+        localstorage[tabid]=data;
+     }
+  });
+});  
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    var open = localstorage[tabId].time;
+    var close  = window.performance && window.performance.now && window.performance.timing && window.performance.timing.navigationStart ? window.performance.now() + window.performance.timing.navigationStart : Date.now();
+    var diff = close - open;
+    var url = localstorage[tabId].url;
+    console.log("Am inchis: "+ url + " dupa "+ diff/1000 + " secunde.");
+    chrome.storage.local.get('userid', function(id) {
+        var data={
+        open: open,
+        close: close,
+        diff: (diff/1000),
+        url:url,
+        host:localstorage[tabId].host,
+        userid: id.userid
+      };
+          (async () => {
+            const rawResponse = await fetch('http://localhost/api/close', {
+              method: 'POST',
+              headers: {
+              // 'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(data)
+            });
+          })();
+      delete localstorage[tabId];
+    });
+     
+});
+chrome.runtime.onInstalled.addListener(function(details){
+  if(details.reason == "install")
+  {
+          userid = getRandomToken();
+          chrome.runtime.getPlatformInfo(function(info) {
+            var data={
+              info: info.os,
+              userid: userid
+            };
+              fetch('http://localhost/api/new', {
+                method: 'POST',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+              });
+        });
+          chrome.storage.local.set({userid: userid}, function() {
+          });
+      
+  }
+  else if(details.reason == "update")
+  {
+      var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+  }
+});
+function getRandomToken() {
+  var randomPool = new Uint8Array(32);
+  crypto.getRandomValues(randomPool);
+  var hex = '';
+  for (var i = 0; i < randomPool.length; ++i) {
+      hex += randomPool[i].toString(16);
+  }
+  return hex;
+}
---
+++ Google Extension/src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import {Pie} from 'react-chartjs-2';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      topcount:[],
+    toptime:[]
+  }
+  }
+  compenentWillMount(){
+    axios.get(`http://localhost/api/topcount/dccd73ac1d46983e87ec42f1a66ba54551e6eb823872247e91c4af857770`)
+    .then(res => {
+      this.setState({ topcount:res.data });
+    });
+    axios.get(`http://localhost/api/toptime/dccd73ac1d46983e87ec42f1a66ba54551e6eb823872247e91c4af857770`)
+    .then(res => {
+      this.setState({ toptime:res.data });
+    });
+  }
+  render() {
+    var labeltime=[];
+    var valuetime=[];
+    this.state.toptime.forEach(element => {
+        labeltime.push(element.host);
+        alert(element.time);
+        valuetime.push(element.time);
+    });
+    var labelcount=[];
+    var valuecount=[];
+    this.state.topcount.forEach(element => {
+        labelcount.push(element.host);
+        valuecount.push(element.count);
+        console.log(element.host);
+    });
+    const datatime = {
+        labels:labeltime,
+        datasets: [{
+            data: valuetime,
+            backgroundColor: [
+            '#d400e2',
+            '#d77ee7',
+            '#741a1a',
+            '#dce543',
+            '#FFEE56'
+            ],
+            hoverBackgroundColor: [
+            '#d400e2',
+            '#d77ee7',
+            '#741a1a',
+            '#dce543',
+            '#FFEE56'
+            ]
+        }]
+    };
+    const datecount = {
+        labels:labelcount,
+        datasets: [{
+            data: valuecount,
+            backgroundColor: [
+            '#d400e2',
+            '#d77ee7',
+            '#741a1a',
+            '#dce543',
+            '#FFEE56'
+            ],
+            hoverBackgroundColor: [
+            '#d400e2',
+            '#d77ee7',
+            '#741a1a',
+            '#dce543',
+            '#FFEE56'
+            ]
+        }]
+    };
+    return (
+      <div className="App">
+              <h2>Top 5 visits on website</h2>
+              <Pie data={datecount} />
+              <h2>Top 5 time on website</h2>
+              <Pie data={datatime} />    
+      </div>
+    );
+  }
+}
+
+export default App;
